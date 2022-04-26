12673475192279cf3f7decd7915a7a3b71e99401
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-04-26 13:26:57 +0700

    create verification page

--- src/pages/Verification.js
+++ src/pages/Verification.js
@@ -3,33 +3,47 @@ import React, { useEffect, useState } from "react";
 import { URL_API } from "../helpers";
 import styles from "./Verification.module.css";
 import { useParams } from "react-router-dom";
+import { MdMarkEmailRead } from "react-icons/md";
 
 function Verification() {
   const params = useParams();
   const [isVerified, setIsVerified] = useState(false);
   console.log(params);
 
-  useEffect(() => {
-    axios
-      .patch(
-        `${URL_API}/users/verify`,
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${params.token}`,
-          },
-        }
-      )
-      .then((res) => {
-        setIsVerified(true);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .patch(
+  //       `${URL_API}/users/verify`,
+  //       {},
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${params.token}`,
+  //         },
+  //       }
+  //     )
+  //     .then((res) => {
+  //       setIsVerified(true);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
   return (
     <div className={styles.container}>
-      {isVerified ? "Verification Success" : "Loading..."}
+      <div className={styles["message-box"]}>
+        <h1 className={styles.title}>Account Activated</h1>
+        <hr />
+        {/* {isVerified ? "Verification Success" : "Loading..."} */}
+        <div className={styles.logo}>
+          <MdMarkEmailRead size={"3em"} color="#2175f3" />
+        </div>
+        <p>
+          Thank you, nama. Your email has been verified and your account is
+          active now. Please use the link below to login to your new account.
+        </p>
+        <button className={styles.button}>Login to Your Account</button>
+        <p>Thank you for joining the Banquet community.</p>
+      </div>
     </div>
   );
 }
