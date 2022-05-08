45d49e55483c8990034a5a1164b4124dc841f564
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-05-09 01:00:07 +0700

    infinite scroll pagination for all-restaurants page

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,16 +1,12 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { URL_API } from "../helpers";
 
-function usePagination(query, pageNumber) {
+function usePagination(pageNumber, endpoint) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  const [data, setData] = useState([]);
+  const [dataRestaurants, setDataRestaurants] = useState([]);
   const [hasMore, setHasMore] = useState(false);
-
-  // Untuk menghapus result query yang lama
-  useEffect(() => {
-    setData([]);
-  }, [query]);
 
   useEffect(() => {
     setLoading(true);
@@ -18,22 +14,26 @@ function usePagination(query, pageNumber) {
     let cancel;
     axios({
       method: "GET",
-      url: "http://openlibrary.org/search.json",
-      params: { q: query, page: pageNumber },
+      url: URL_API + endpoint,
+      params: { page: pageNumber },
       // cancelToken kita gunakan sebagai cleanup effect
       // jadi disini usePagination ga akan dijalankan setiap query berubah
       // dia menerima parameter berupa function yang akan mengubah cancel variable jadi c
       cancelToken: new axios.CancelToken((c) => (cancel = c)),
     })
       .then((res) => {
-        setData((data) => {
-          // set digunakan untuk remove all duplicate di dalam array
-          return [...new Set([...data, ...res.data.docs.map((b) => b.title)])];
+        console.log(res.data);
+        setDataRestaurants((dataRestaurants) => {
+          return [...dataRestaurants, ...res.data.dataUser];
         });
         // Ini di cek apa masih ada data yang belum di load
-        setHasMore(res.data.docs.length > 0);
+        console.log(res.data.token[0].totalRestaurants);
+        setHasMore(
+          // dataRestaurants.length < res.data.token[0].totalRestaurants - 1
+          res.data.dataUser.length > 0
+        );
         setLoading(false);
-        console.log(res.data);
+        // console.log(res.data);
       })
       .catch((e) => {
         // Ini artinya jika error dari cancel token maka gpp karena memang sengaja kita setting untuk error
@@ -42,8 +42,58 @@ function usePagination(query, pageNumber) {
       });
     // untuk menggunakan cleanup effect maka kita perlu return sebuah function dalam useEffect
     return () => cancel();
-  }, [query, pageNumber]);
-  return { loading, error, data, hasMore };
+  }, [pageNumber]);
+  return { loading, error, dataRestaurants, hasMore };
 }
 
 export default usePagination;
+
+// import axios from "axios";
+// import { useState, useEffect } from "react";
+
+// function usePagination(query, pageNumber) {
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(false);
+//   const [data, setData] = useState([]);
+//   const [hasMore, setHasMore] = useState(false);
+
+//   // Untuk menghapus result query yang lama
+//   useEffect(() => {
+//     setData([]);
+//   }, [query]);
+
+//   useEffect(() => {
+//     setLoading(true);
+//     setError(false);
+//     let cancel;
+//     axios({
+//       method: "GET",
+//       url: "http://openlibrary.org/search.json",
+//       params: { q: query, page: pageNumber },
+//       // cancelToken kita gunakan sebagai cleanup effect
+//       // jadi disini usePagination ga akan dijalankan setiap query berubah
+//       // dia menerima parameter berupa function yang akan mengubah cancel variable jadi c
+//       cancelToken: new axios.CancelToken((c) => (cancel = c)),
+//     })
+//       .then((res) => {
+//         setData((data) => {
+//           // set digunakan untuk remove all duplicate di dalam array
+//           return [...new Set([...data, ...res.data.docs.map((b) => b.title)])];
+//         });
+//         // Ini di cek apa masih ada data yang belum di load
+//         setHasMore(res.data.docs.length > 0);
+//         setLoading(false);
+//         console.log(res.data);
+//       })
+//       .catch((e) => {
+//         // Ini artinya jika error dari cancel token maka gpp karena memang sengaja kita setting untuk error
+//         if (axios.isCancel(e)) return;
+//         setError(true);
+//       });
+//     // untuk menggunakan cleanup effect maka kita perlu return sebuah function dalam useEffect
+//     return () => cancel();
+//   }, [query, pageNumber]);
+//   return { loading, error, data, hasMore };
+// }
+
+// export default usePagination;
--- src/pages/Restaurants.js
+++ src/pages/Restaurants.js
@@ -1,37 +1,32 @@
-import React, { useEffect, useState, useRef, useCallback } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import styles from "./Restaurants.module.css";
-
-import axios from "axios";
 import { URL_API } from "../helpers";
 import { Link } from "react-router-dom";
 import usePagination from "../hooks/usePagination";
 import RestaurantCard from "../components/RestaurantCard";
+import Spinner from "../components/Spinner";
 
 function Restaurants() {
-  const [restaurants, setRestaurants] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
 
-  const [query, setQuery] = useState("");
-  const [pageNumber, setPageNumber] = useState("");
+  const { dataRestaurants, hasMore, loading, error } = usePagination(
+    pageNumber,
+    "/restaurants"
+  );
 
-  const { data, hasMore, loading, error } = usePagination(query, pageNumber);
+  console.log(dataRestaurants);
 
-  // useRef kita gunakan untuk menyimpan suatu nilai yang kita ingin untuk persist di dalam rerender process
-  // useRef juga bisa digunakan untuk membuat reference ke DOM element
-  // useRef juga bisa digunakan untuk membuat reference ke DOM API
-  // Disini kita gunakan useRef untuk membuat reference ke DOM API, dalam hal ini intersection observer. Dia digunakan untuk mereference ke hasil query terakhir kita. Namun karena dia hanya reference, dia ga akan mengakibatkan rerender kalau intersection observer sudah mencapai hasil terakhir. Untuk itu kita perlu tambahan useCallback.
   const observer = useRef();
-
-  // Kita kasih reference dengan useCallback ke element terakhir. Jadi ketika kapanpun element ini ter create, function di dalam useCallback akan dijalankan
   const lastDataElementRef = useCallback(
     (node) => {
       // Kalau kita sedang loading maka kita gamau trigger infinite scroll, makanya di return
       if (loading) return;
       // Jika kita sudah ketemu last element, maka disconnect attribute last elementnya
-      if (observer.return) observer.current.disconnect();
+      if (observer.current) observer.current.disconnect();
 
       observer.current = new IntersectionObserver((entries) => {
         if (entries[0].isIntersecting && hasMore) {
-          // console.log("visible");
+          console.log("visible");
           setPageNumber((pageNumber) => pageNumber + 1);
         }
       });
@@ -42,69 +37,46 @@ function Restaurants() {
     [loading, hasMore]
   );
 
-  usePagination(query, pageNumber);
-
-  function handleSearch(e) {
-    setQuery(e.target.value);
-    setPageNumber(1);
-  }
-
-  useEffect(() => {
-    axios
-      .get(URL_API + "/restaurants")
-      .then((res) => {
-        console.log(res.data);
-        setRestaurants(res.data.dataUser);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
-  // console.log(restaurants);
-
-  // console.log(restaurants);
-  const generateRestaurantsCard = () => {
-    let rawData = [...restaurants];
-
-    // const dataPaginated = rawData.slice(
-    //   beginningIndex,
-    //   beginningIndex + itemPerPage
-    // );
-
-    return rawData.map((restaurant, index) => {
-      return (
-        <RestaurantCard
-          name={restaurant.name}
-          image={URL_API + restaurant.restaurantImageUrl}
-          description={restaurant.description}
-          key={restaurant.restaurantId}
-          id={restaurant.restaurantId}
-        />
-      );
-    });
-  };
+  console.log(hasMore);
+  console.log(loading);
+  console.log(error);
+  console.log(pageNumber);
 
   return (
     <div className={styles.container}>
-      <input type="text" onChange={handleSearch} value={query} />
-      {data.map((item, index) => {
-        if (data.length === index + 1) {
-          return (
-            <div ref={lastDataElementRef} key={item}>
-              {item}
-            </div>
-          );
-        } else {
-          return <div key={item}>{item}</div>;
-        }
-      })}
-      <div>{loading && "Loading..."}</div>
-      <div>{error && "Error..."}</div>
+      {loading ? <Spinner /> : ""}
       <div className={styles["data-render"]}>
         <div className={styles.title}>All Restaurants</div>
         <div className={styles["cards-container"]}>
-          {generateRestaurantsCard()}
+          {dataRestaurants.map((restaurant, index) => {
+            if (dataRestaurants.length === index + 1) {
+              return (
+                <div ref={lastDataElementRef}>
+                  <RestaurantCard
+                    name={restaurant.name}
+                    image={URL_API + restaurant.restaurantImageUrl}
+                    description={restaurant.description}
+                    key={restaurant.restaurantId}
+                    id={restaurant.restaurantId}
+                  />
+                </div>
+              );
+            } else {
+              return (
+                <div>
+                  <RestaurantCard
+                    name={restaurant.name}
+                    image={URL_API + restaurant.restaurantImageUrl}
+                    description={restaurant.description}
+                    key={restaurant.restaurantId}
+                    id={restaurant.restaurantId}
+                  />
+                </div>
+              );
+            }
+          })}
+          {/* <div>{loading && "Loading..."}</div>
+          <div>{error && "Error..."}</div> */}
         </div>
       </div>
     </div>
@@ -112,3 +84,118 @@ function Restaurants() {
 }
 
 export default Restaurants;
+
+// import React, { useEffect, useState, useRef, useCallback } from "react";
+// import styles from "./Restaurants.module.css";
+
+// import axios from "axios";
+// import { URL_API } from "../helpers";
+// import { Link } from "react-router-dom";
+// import usePagination from "../hooks/usePagination";
+// import RestaurantCard from "../components/RestaurantCard";
+
+// function Restaurants() {
+//   const [restaurants, setRestaurants] = useState("");
+
+//   const [query, setQuery] = useState("");
+//   const [pageNumber, setPageNumber] = useState("");
+
+//   const { data, hasMore, loading, error } = usePagination(query, pageNumber);
+
+//   // useRef kita gunakan untuk menyimpan suatu nilai yang kita ingin untuk persist di dalam rerender process
+//   // useRef juga bisa digunakan untuk membuat reference ke DOM element
+//   // useRef juga bisa digunakan untuk membuat reference ke DOM API
+//   // Disini kita gunakan useRef untuk membuat reference ke DOM API, dalam hal ini intersection observer. Dia digunakan untuk mereference ke hasil query terakhir kita. Namun karena dia hanya reference, dia ga akan mengakibatkan rerender kalau intersection observer sudah mencapai hasil terakhir. Untuk itu kita perlu tambahan useCallback.
+//   const observer = useRef();
+
+//   // Kita kasih reference dengan useCallback ke element terakhir. Jadi ketika kapanpun element ini ter create, function di dalam useCallback akan dijalankan
+//   const lastDataElementRef = useCallback(
+//     (node) => {
+//       // Kalau kita sedang loading maka kita gamau trigger infinite scroll, makanya di return
+//       if (loading) return;
+//       // Jika kita sudah ketemu last element, maka disconnect attribute last elementnya
+//       if (observer.current) observer.current.disconnect();
+
+//       observer.current = new IntersectionObserver((entries) => {
+//         if (entries[0].isIntersecting && hasMore) {
+//           // console.log("visible");
+//           setPageNumber((pageNumber) => pageNumber + 1);
+//         }
+//       });
+//       // Jika kita ketemu last element maka pastikan observer to observe it
+//       if (node) observer.current.observe(node);
+//       console.log(node);
+//     },
+//     [loading, hasMore]
+//   );
+
+//   usePagination(query, pageNumber);
+
+//   function handleSearch(e) {
+//     setQuery(e.target.value);
+//     setPageNumber(1);
+//   }
+
+//   useEffect(() => {
+//     axios
+//       .get(URL_API + "/restaurants")
+//       .then((res) => {
+//         console.log(res.data);
+//         setRestaurants(res.data.dataUser);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }, []);
+
+//   // console.log(restaurants);
+
+//   // console.log(restaurants);
+//   const generateRestaurantsCard = () => {
+//     let rawData = [...restaurants];
+
+//     // const dataPaginated = rawData.slice(
+//     //   beginningIndex,
+//     //   beginningIndex + itemPerPage
+//     // );
+
+//     return rawData.map((restaurant, index) => {
+//       return (
+//         <RestaurantCard
+//           name={restaurant.name}
+//           image={URL_API + restaurant.restaurantImageUrl}
+//           description={restaurant.description}
+//           key={restaurant.restaurantId}
+//           id={restaurant.restaurantId}
+//         />
+//       );
+//     });
+//   };
+
+//   return (
+//     <div className={styles.container}>
+//       <input type="text" onChange={handleSearch} value={query} />
+//       {data.map((item, index) => {
+//         if (data.length === index + 1) {
+//           return (
+//             <div ref={lastDataElementRef} key={item}>
+//               {item}
+//             </div>
+//           );
+//         } else {
+//           return <div key={item}>{item}</div>;
+//         }
+//       })}
+//       <div>{loading && "Loading..."}</div>
+//       <div>{error && "Error..."}</div>
+//       <div className={styles["data-render"]}>
+//         <div className={styles.title}>All Restaurants</div>
+//         <div className={styles["cards-container"]}>
+//           {generateRestaurantsCard()}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Restaurants;
