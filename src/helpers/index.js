deacf6bd4a008bbca95b6dc078d9d074453762eb
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-08-10 15:21:42 +0700

    redirect bug

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,3 @@
-// export const URL_API = "http://localhost:2000";
+export const URL_API = "http://localhost:2000";
 
-export const URL_API = "https://banquet-app.herokuapp.com";
+// export const URL_API = "https://banquet-app.herokuapp.com";
